e09b4b24624a1e48d1bf2d718cf6354ba0ee4892
Hoai Duc <hoaiduc2000@gmail.com>
2016-06-29 16:32:22 +0700

    Navigation Drawer Update 1

---
+++ app/src/main/java/view/CircleImageView.java
@@ -0,0 +1,45 @@
+package view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.widget.ImageView;
+
+import com.example.nguyenhoaiduc.layout_pratice_2.R;
+
+/**
+ * Created by nguyen.hoai.duc on 6/29/2016.
+ */
+public class CircleImageView  {
+       Context mContext;
+    public CircleImageView(Context context) {
+        this.mContext = context;
+    }
+    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+        int targetWidth = 200;
+        int targetHeight = 200;
+        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
+                targetHeight,Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(targetBitmap);
+        Path path = new Path();
+        path.addCircle(((float) targetWidth - 1) / 2,
+                ((float) targetHeight - 1) / 2,
+                (Math.min(((float) targetWidth),
+                        ((float) targetHeight)) / 2),
+                Path.Direction.CCW);
+
+        canvas.clipPath(path);
+        Bitmap sourceBitmap = scaleBitmapImage;
+        canvas.drawBitmap(sourceBitmap,
+                new Rect(0, 0, sourceBitmap.getWidth(),
+                        sourceBitmap.getHeight()),
+                new Rect(0, 0, targetWidth, targetHeight), null);
+        return targetBitmap;
+    }
+}
